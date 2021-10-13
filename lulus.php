271310a0683ef3244fc9e34bb665cbd06d0adecc
nafisuling <nafisuling@gmail.com>
2021-10-13 09:35:02 +0700

    added lulus and tidak lulus page

---
+++ lulus.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<center>
+	<br><br><br><br>
+	<h1> SELAMAT ANDA LULUS LDKO 2021 </h1>
+	</center>
+</body>
+</html>
---
+++ tidaklulus.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<center>
+	<h1> Mohon Maaf Anda Belum Lulus </h1>
+	</center>
+</body>
+</html>
